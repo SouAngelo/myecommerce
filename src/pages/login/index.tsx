95c8cf0655d20c5c5b816e10d40e1004fa1d8577
Angelo Gustavo <angelotj43@gmail.com>
2023-04-13 14:04:17 -0300

    att project

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,45 +1,45 @@
-// import React, { useState } from 'react'
-// import Link from 'next/link'
-// import { signIn } from 'next-auth/react'
-// import { FaGoogle } from 'react-icons/fa'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { signIn } from 'next-auth/react'
+import { FaGoogle } from 'react-icons/fa'
 
-// function Login() {
+function Login() {
 
-//     const [email, setEmail] = useState('')
-//     const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-//   return (
-//     <div className='container'>
+  return (
+    <div className='container'>
       
-//       <form className='formContainer'>
-//           <div className='formHeader'>
-//             <div><span>SHOP</span>NEW <img src="https://cdn-icons-png.flaticon.com/512/2271/2271049.png" alt="" /></div>
+      <form className='formContainer'>
+          <div className='formHeader'>
+            <div><span>SHOP</span>NEW <img src="https://cdn-icons-png.flaticon.com/512/2271/2271049.png" alt="" /></div>
 
-//             <h2>Login</h2>
-//             <p>Logue para acessar</p>
-//           </div>
+            <h2>Login</h2>
+            <p>Logue para acessar</p>
+          </div>
 
-//           <div className='inputContainer'>
-//             {/* <label htmlFor="user">Email</label>
-//             <input type="email" placeholder="email@email.com" name="user"/>
+          <div className='inputContainer'>
+            {/* <label htmlFor="user">Email</label>
+            <input type="email" placeholder="email@email.com" name="user"/>
 
-//             <label htmlFor="pass">Senha</label>
-//             <input type="password" placeholder="********" name="pass"/>
+            <label htmlFor="pass">Senha</label>
+            <input type="password" placeholder="********" name="pass"/>
 
-//             <button type="submit">Logar</button>
+            <button type="submit">Logar</button>
 
-//             <p>Não tem uma conta? <Link href='/register' legacyBehavior><a>Cadastre-se</a></Link></p> */}
+            <p>Não tem uma conta? <Link href='/register' legacyBehavior><a>Cadastre-se</a></Link></p> */}
 
-//             {/* <p>Site sem funcionar, estamos em atualizações... </p> */}
+            {/* <p>Site sem funcionar, estamos em atualizações... </p> */}
 
-//             <div className='authGoogle' onClick={() => signIn()}>
-//             <FaGoogle size={25}/> <p>Continuar com google</p> 
-//             </div>
-//           </div>
+            <div className='authGoogle' onClick={() => signIn()}>
+            <FaGoogle size={25}/> <p>Continuar com google</p> 
+            </div>
+          </div>
 
-//         </form>
-//     </div>
-//   )
-// }
+        </form>
+    </div>
+  )
+}
 
-// export default Login
+export default Login
